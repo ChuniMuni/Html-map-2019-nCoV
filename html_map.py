696d9ecf566a129c90f2052a66a32e5305253177
ChuniMuni <tstvitali@gmail.com>
2020-02-24 11:56:57 +0300

    Link initialization.

--- html_map.py
+++ html_map.py
@@ -2,3 +2,7 @@ import plotly.offline as go_offline
 import plotly.graph_objects as go
 import pandas as pd
 
+url = 'https://docs.google.com/spreadsheets/d/18X1VM1671d99V_yd-cnUI1j8oSG2ZgfU_q1HfOizErA/export?format=csv&id'
+data = pd.read_csv(url)
+data = data.fillna(0)
+
