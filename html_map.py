a945dc5502cf155c62ce5a431e22d795ea1d1f3f
ChuniMuni <tstvitali@gmail.com>
2020-02-24 13:02:46 +0300

    Variable initialization Add cycle for data analysis and preparation Scattergeo chart customization

--- html_map.py
+++ html_map.py
@@ -6,3 +6,38 @@ url = 'https://docs.google.com/spreadsheets/d/18X1VM1671d99V_yd-cnUI1j8oSG2ZgfU_
 data = pd.read_csv(url)
 data = data.fillna(0)
 
+# Variable initialization
+fig=go.Figure()
+col_name=data.columns
+n_col=len(data.columns)
+date_list=[]
+init=4
+n_range=int((n_col-5)/2)
+
+# The cycle for data analysis and preparation
+for i in range(n_range):
+    col_case = init+1
+    col_dead = col_case+1
+    init = col_case+1
+    df_split = data[['latitude', 'longitude', 'country', 'location', col_name[col_case], col_name[col_dead]]]
+    df = df_split[(df_split[col_name[col_case]] != 0)]
+    lat = df['latitude']
+    lon = df['longitude']
+    case = df[df.columns[-2]].astype(int)
+    deaths = df[df.columns[-1]].astype(int)
+    df['text'] = df['country'] + '<br>' + df['location'] + '<br>' + 'confirmed cases: ' + case.astype(str) + '<br>' + 'deaths: ' + deaths.astype(str)
+    date_label = deaths.name[7:17]
+    date_list.append(date_label)
+
+    # Scattergeo chart customization
+    fig.add_trace(go.Scattergeo(
+    name = '',
+    lon = lon,
+    lat = lat,
+    visible = False,
+    hovertemplate = df['text'],
+    text = df['text'],
+    mode = 'markers',
+    marker = dict(size=15, opacity=0.6, color='Red', symbol='circle'),
+    ))
+
